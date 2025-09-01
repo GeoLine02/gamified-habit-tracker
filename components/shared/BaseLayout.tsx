05cdd6f939fc0931a0a81a71ac4fbb7f328027a0
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2025-09-02 00:27:05 +0400

    hotfix: gap issue

--- components/shared/BaseLayout.tsx
+++ components/shared/BaseLayout.tsx
@@ -7,7 +7,7 @@ export default function BaseLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex flex-col min-h-screen pb-4 gap-6">
+    <div className="flex flex-col min-h-screen pb-4 gap-6ga">
       <Header />
       <main className="flex-1 flex flex-col">{children}</main>
       <Footer />
