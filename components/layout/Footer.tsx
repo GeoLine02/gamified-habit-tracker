ffb4c60b1146d506b7782d2412b185d82994ff29
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2025-09-02 00:38:08 +0400

    hotfix: avtive nav link styles

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,11 +1,26 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Footer = () => {
+  const pathName = usePathname();
+
+  const activeLinkStyles =
+    "bg-custom-green text-white py-2 px-6 rounded-xl transition-all duration-200 ease-in-out";
+
   return (
-    <footer className="md:hidden px-4 ">
+    <footer className="md:hidden px-4">
       <div className="flex items-center justify-around bg-white py-4 rounded-xl font-medium">
-        <Link href={"/"}>Dashboard</Link>
-        <Link href={"/profile"}>Profile</Link>
+        <Link className={`${pathName === "/" && activeLinkStyles}`} href={"/"}>
+          Dashboard
+        </Link>
+        <Link
+          className={`${pathName === "/profile" && activeLinkStyles}`}
+          href={"/profile"}
+        >
+          Profile
+        </Link>
       </div>
     </footer>
   );
